6b0053e7e23b13952dc01959e6cc63d8c1c9324e
up4k <mailbox73@mail.ru>
2024-05-14 05:51:28 +0400

    вроде переключение работает надо тестить

--- browser.py
+++ browser.py
@@ -1,25 +1,33 @@
-# browser.py
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from fake_useragent import UserAgent
+from configureproxy import parse_proxy, configure_proxy
 
 def get_random_user_agent():
     ua = UserAgent()
     return ua.random
 
-def create_firefox_browser():
+def create_firefox_browser(proxy=None):
     path_to_geckodriver = './geckodriver-0.34.0/geckodriver-0.34.0/'  # Укажите путь к geckodriver
     service = Service(executable_path=path_to_geckodriver)
     options = Options()
     options.set_preference("media.peerconnection.enabled", False)
     options.set_preference("media.navigator.permission.disabled", True)
     options.set_preference("media.navigator.streams.fake", True)
-    options.set_preference("network.proxy.type", 1)
-    options.set_preference("network.proxy.socks", "192.168.3.238")
-    options.set_preference("network.proxy.socks_port", 3067)
-    options.set_preference("network.proxy.socks_version", 5)
-    options.set_preference("network.proxy.socks_remote_dns", True)
+    
+    if proxy:
+        proxy_type, ip, port, user, password = parse_proxy(proxy)
+        if proxy_type and ip and port:
+            configure_proxy(options, proxy_type, ip, port, user, password)
+    else:
+        # Задайте здесь свои прокси по умолчанию
+        options.set_preference("network.proxy.type", 1)
+        options.set_preference("network.proxy.socks", "192.168.3.238")
+        options.set_preference("network.proxy.socks_port", 3067)
+        options.set_preference("network.proxy.socks_version", 5)
+        options.set_preference("network.proxy.socks_remote_dns", True)
+    
     user_agent = get_random_user_agent()
     options.set_preference("general.useragent.override", user_agent)
     driver = webdriver.Firefox(service=service, options=options)
--- configureproxy.py
+++ configureproxy.py
@@ -1,5 +1,6 @@
+import logging
+
 def load_proxy_list(file_path):
-    """ Загружает список прокси из файла """
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             proxies = [line.strip() for line in file if line.strip()]
@@ -9,7 +10,6 @@ def load_proxy_list(file_path):
         return []
 
 def parse_proxy(proxy_string):
-    """ Разбирает строку прокси и возвращает тип прокси, IP, порт, имя пользователя и пароль (если есть) """
     try:
         if '@' in proxy_string:
             credentials, address = proxy_string.split('@')
@@ -22,28 +22,34 @@ def parse_proxy(proxy_string):
             proxy_type = address.split('://')[0]
             ip_port = address.split('://')[1]
         else:
-            # Default to http if not specified
             proxy_type = 'http'
             ip_port = address
 
-        # Разбиваем адрес на IP и порт, предполагая, что порт всегда последний после последнего двоеточия
         *ip_parts, port = ip_port.split(':')
-        ip = ':'.join(ip_parts)  # Это позволит корректно обработать IPv6
+        ip = ':'.join(ip_parts)
 
         return proxy_type, ip, int(port), user, password
     except Exception as e:
         logging.error(f"Error parsing proxy {proxy_string}: {str(e)}")
         return None, None, None, None, None
 
-
 def configure_proxy(options, proxy_type, ip, port, username=None, password=None):
-    """ Настраивает прокси для Selenium Options """
+    options.set_preference("network.proxy.type", 1)
     if proxy_type == 'socks5':
-        options.add_argument(f'--proxy-server=socks5://{ip}:{port}')
+        options.set_preference("network.proxy.socks", ip)
+        options.set_preference("network.proxy.socks_port", port)
+        options.set_preference("network.proxy.socks_version", 5)
+        options.set_preference("network.proxy.socks_remote_dns", True)
     elif proxy_type == 'socks4':
-        options.add_argument(f'--proxy-server=socks4://{ip}:{port}')
+        options.set_preference("network.proxy.socks", ip)
+        options.set_preference("network.proxy.socks_port", port)
+        options.set_preference("network.proxy.socks_version", 4)
     elif proxy_type == 'http':
-        options.add_argument(f'--proxy-server={ip}:{port}')
+        options.set_preference("network.proxy.http", ip)
+        options.set_preference("network.proxy.http_port", port)
+        options.set_preference("network.proxy.ssl", ip)
+        options.set_preference("network.proxy.ssl_port", port)
 
     if username and password:
-        options.add_argument(f'--proxy-auth={username}:{password}')
+        options.set_preference("network.proxy.username", username)
+        options.set_preference("network.proxy.password", password)
